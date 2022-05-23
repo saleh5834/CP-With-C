474b26478a359362a495941e6b7c2ae1979fe358
saleh5834 <salehahmad.cse@gmail.com>
2022-05-23 21:00:36 +0600

    first commit

---
+++ 1st Class Task/Class 1.c.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=11,result;
+	result=((a+b)/3)*2;
+	printf("Print The Value of Result=%d",result);
+	return 0;
+}
---
+++ 1st Class Task/main.c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ei bristy bheja rate \nTumi nei bole");
+    return 0;
+}
